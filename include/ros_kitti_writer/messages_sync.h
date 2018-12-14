86e217fa06ffb2918dabb721c1ad0831a1e04576
zhanghming <zhanghm_1995@qq.com>
2018-12-14 14:13:57 +0800

    add StereoMessageSync class

--- include/ros_kitti_writer/messages_sync.h
+++ include/ros_kitti_writer/messages_sync.h
@@ -22,35 +22,86 @@
 #include <message_filters/sync_policies/approximate_time.h>
 
 namespace sensors_fusion {
+
 /**
  * @brief     Class for synchronizing sensor_msg::Image and sensor_msgs::PointCloud2.
  */
 class MessagesSync {
 public:
   typedef  std::pair<sensor_msgs::ImageConstPtr, sensor_msgs::PointCloud2ConstPtr> SyncImageCloudPair;
+
   // Cons
 //  MessagesSync(){};
   MessagesSync(ros::NodeHandle nh, std::string camera_topic_name, std::string lidar_topic_name);
+  MessagesSync(ros::NodeHandle nh, std::string camera1_topic_name, std::string camera2_topic_name,
+               std::string lidar_topic_name);
   virtual ~MessagesSync();
 
   SyncImageCloudPair getSyncMessages();
 
 private:
   void cameraLidarCallback(const sensor_msgs::ImageConstPtr& image_msg,const sensor_msgs::PointCloud2ConstPtr& lidar_msg);
+
 private:
   //! ROS node handle.
   ros::NodeHandle nodeHandle_;
 
   // 消息过滤器订阅相机和激光雷达点云话题
   typedef message_filters::Subscriber<sensor_msgs::Image> subCameraImage;
-  subCameraImage subCameraImage_;//订阅图像消息
+  subCameraImage subCamera1Image_;//订阅图像消息
   typedef message_filters::Subscriber<sensor_msgs::PointCloud2> subLidarData;
   subLidarData subLidarData_;//订阅激光雷达消息
+
   typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,sensor_msgs::PointCloud2> MySyncPolicy;
   message_filters::Synchronizer< MySyncPolicy > sync;
 
+
   // returns
   SyncImageCloudPair syncMessages_;
+  bool flag;
+};
+
+
+
+/**
+ * @brief     Class for synchronizing sensor_msg::Image and sensor_msgs::PointCloud2.
+ */
+class StereoMessagesSync {
+public:
+  typedef struct SynchronizedMessages {
+    sensor_msgs::ImageConstPtr image1_ptr;
+    sensor_msgs::ImageConstPtr image2_ptr;
+    sensor_msgs::PointCloud2ConstPtr cloud_ptr;
+  }SynchronizedMessages;
+
+  // Cons
+//  MessagesSync(){};
+  StereoMessagesSync(ros::NodeHandle nh, std::string camera1_topic_name, std::string camera2_topic_name,
+               std::string lidar_topic_name);
+  virtual ~StereoMessagesSync();
+
+  SynchronizedMessages getSyncMessages();
+
+private:
+  void stereocameraLidarCallback(const sensor_msgs::ImageConstPtr& image1_msg,
+                               const sensor_msgs::ImageConstPtr& image2_msg,
+                               const sensor_msgs::PointCloud2ConstPtr& lidar_msg);
+private:
+  //! ROS node handle.
+  ros::NodeHandle nodeHandle_;
+
+  // 消息过滤器订阅相机和激光雷达点云话题
+  typedef message_filters::Subscriber<sensor_msgs::Image> subCameraImage;
+  subCameraImage subCamera1Image_;//订阅图像消息
+  subCameraImage subCamera2Image_;//订阅图像消息
+  typedef message_filters::Subscriber<sensor_msgs::PointCloud2> subLidarData;
+  subLidarData subLidarData_;//订阅激光雷达消息
+
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,sensor_msgs::Image, sensor_msgs::PointCloud2> MySyncPolicy;
+  message_filters::Synchronizer< MySyncPolicy> sync;
+
+  // returns
+  SynchronizedMessages syncMessages2_;
   bool flag;
 };
 
--- src/messages_sync.cpp
+++ src/messages_sync.cpp
@@ -19,12 +19,11 @@ namespace sensors_fusion {
 ////////////////////////////////MessagesSync////////////////////////////////////////////////////////
 MessagesSync::MessagesSync(ros::NodeHandle nh, std::string camera_topic_name, std::string lidar_topic_name):
     nodeHandle_(nh),
-    subCameraImage_(nh, camera_topic_name, 2),
+    subCamera1Image_(nh, camera_topic_name, 2),
     subLidarData_(nh,lidar_topic_name, 2),
     flag(false),
-    sync(MySyncPolicy(10), subCameraImage_, subLidarData_)
+    sync(MySyncPolicy(10), subCamera1Image_, subLidarData_)
 {
-  //发布同步后的数据
   ROS_INFO("lidar and camera data synchronizing started");
   sync.registerCallback(boost::bind(&MessagesSync::cameraLidarCallback, this,_1, _2));
 }
@@ -39,6 +38,9 @@ MessagesSync::SyncImageCloudPair MessagesSync::getSyncMessages()
   }
 }
 
+
+
+
 void MessagesSync::cameraLidarCallback(const sensor_msgs::ImageConstPtr& image_msg,const sensor_msgs::PointCloud2ConstPtr& lidar_msg)
 {
   ROS_INFO_THROTTLE(5, "Sync_Callback");
@@ -50,7 +52,53 @@ void MessagesSync::cameraLidarCallback(const sensor_msgs::ImageConstPtr& image_m
   flag = true;
 }
 
+
+
 MessagesSync::~MessagesSync() {
+}
+
+
+/////////////////////////////////StereoMessagesSync//////////////////////////////////////////////////////////
+
+StereoMessagesSync::StereoMessagesSync(ros::NodeHandle nh, std::string camera1_topic_name,
+                                       std::string camera2_topic_name,
+                                       std::string lidar_topic_name):
+nodeHandle_(nh),
+subCamera1Image_(nh, camera1_topic_name, 2),
+subLidarData_(nh,lidar_topic_name, 2),
+flag(false),
+sync(MySyncPolicy(10), subCamera1Image_, subCamera2Image_, subLidarData_)
+{
+  ROS_INFO("lidar and stereo camera data synchronizing started");
+  sync.registerCallback(boost::bind(&StereoMessagesSync::stereocameraLidarCallback, this,_1, _2, _3));
+}
+
+StereoMessagesSync::~StereoMessagesSync() {
+}
+
+
+void StereoMessagesSync::stereocameraLidarCallback(const sensor_msgs::ImageConstPtr& image1_msg,
+                               const sensor_msgs::ImageConstPtr& image2_msg,
+                               const sensor_msgs::PointCloud2ConstPtr& lidar_msg)
+{
+  sensor_msgs::PointCloud2Ptr cloudMsg(new sensor_msgs::PointCloud2(*lidar_msg));
+  cloudMsg->fields[3].name = "intensity";
+  SynchronizedMessages result;
+  result.image1_ptr = image1_msg;
+  result.image2_ptr = image2_msg;
+  result.cloud_ptr = cloudMsg;
+  syncMessages2_ = result;
+  flag = true;
+}
+
+StereoMessagesSync::SynchronizedMessages  StereoMessagesSync::getSyncMessages()
+{
+  if(!flag)
+    return SynchronizedMessages();
+  else {
+    flag = false;
+    return syncMessages2_;
+  }
 }
 
 } /* namespace sensors_fusion */
